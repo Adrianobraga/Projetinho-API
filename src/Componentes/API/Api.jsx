5b71ec3619df9516b27a1a193d55dc9b9220b491
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-28 22:44:23 -0300

    Modificação para que as variaveis ficaquem no App

--- src/Componentes/API/Api.jsx
+++ src/Componentes/API/Api.jsx
@@ -1,16 +1,6 @@
 import { useState } from "react";
 
-function Api({ name }) {
-     //birth_year
-    //eye_color
-    //gender
-    //hair_color
-    //height
-    //homeworld
-    //mass
-    //name
-    //skin_color   
-  const [people,setPeople] = useState({})
+function Api({ name,setPessoa }) {
   const busca = (name) => {
     fetch(`https://swapi.dev/api/people/?search=${name}`)
       .then((resposta) => {
@@ -18,7 +8,7 @@ function Api({ name }) {
         if (contentType && contentType.indexOf("application/json") !== -1) {
           return resposta.json().then((json) => {
             if (json.results[0].name == name) {
-              setPeople(json.results[0])
+              setPessoa(json.results[0]);
             }
           });
         }
@@ -27,24 +17,7 @@ function Api({ name }) {
         console.error("Erro na requisição" + error);
       });
   };
-  return(
-    <>
-    {busca(name)}
-    <div className="border">
-      Nome:{people.name}  
-    <p>{people.birth_year}  </p>
-    <p>{people.skin_color}  </p>
-
-    <p>{people.eye_color} </p> 
-    <p>{people.gender}  </p>
-    <p>{people.hair_color}  </p>
-    <p>{people.height}  </p>
-    <p>{people.mass}  </p>
-
-    </div>
-
-    </>
-  )
+ return (busca(name));
 }
- 
+
 export default Api;
