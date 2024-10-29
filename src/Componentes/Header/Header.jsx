b6d4966b0ba7c6012f088fa72a9d169d7a3d080e
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-28 22:46:46 -0300

    Enviar o nome do personagem

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,7 +1,16 @@
-function Header({ name }) {
+function Header({ name,pessoa,adicionarElemento }) {
+  const handleSubmit = (e) => {
+    adicionarElemento(pessoa)
+    e.preventDefault();
+    e.target.reset();
+  };
   return (
+    
     <>
-       <input type="text" className="border border-current rounded-2xl" onKeyUp={(e) => name(e.target.value)}/>
+      <form onSubmit={handleSubmit}>
+       <input type="text" className="border border-current rounded-2xl" onKeyUp={(e) => name(e.target.value)} required/>
+       <button type="submit" className="border-solid border-2 bg-gray-300">Enviar</button>
+       </form>
     </>
   );
 }
