e0aef799e3e3225cad0bdc9d56d9abe997d354da
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-30 16:45:52 -0300

    Problema em execultar a função busca

--- src/Componentes/API/Api.jsx
+++ src/Componentes/API/Api.jsx
@@ -1,15 +1,17 @@
-function Api({ name,setPessoa,escolha}) {
+import { useState } from "react";
+
+function Api({ fixo,setPessoa,setFixo }) {
+  console.log(fixo)
   const busca = (name) => {
-    fetch(`https://swapi.dev/api/${(escolha == "Personagem")?"people":(escolha == "Planeta")?"planets":(escolha == "naves estelares")?"starships":"Falhou"}/?search=${name}`)
+    fetch(`https://swapi.dev/api/people/?search=${name}`)
       .then((resposta) => {
         let contentType = resposta.headers.get("content-type");
         if (contentType && contentType.indexOf("application/json") !== -1) {
           return resposta.json().then((json) => {
             if (json.results[0].name == name) {
               setPessoa(json.results[0]);
-              setTimeout(() =>{
-                setPessoa({})
-              },3000)
+             setFixo("")
+            
             }
           });
         }
@@ -17,9 +19,8 @@ function Api({ name,setPessoa,escolha}) {
       .catch((error) => {
         console.error("Erro na requisição" + error);
       });
-
   };
- return (busca(name));
+ return (busca(fixo));
 }
 
 export default Api;
