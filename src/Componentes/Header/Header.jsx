b25ea6078b9bfc4ab5205995db3e512396e5de0c
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-29 21:03:16 -0300

    Escolhas adicionada personagem,planeta e naves

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,16 +1,25 @@
-function Header({ name,pessoa,adicionarElemento }) {
+function Header({ name,pessoa,adicionarElemento,setEscolha }) {
   const handleSubmit = (e) => {
     adicionarElemento(pessoa)
     e.preventDefault();
     e.target.reset();
   };
+
   return (
     
     <>
+    <div className="flex justify-center bg-slate-200 h-40 py-16">
       <form onSubmit={handleSubmit}>
-       <input type="text" className="border border-current rounded-2xl" onChange={(e) => name(e.target.value)} required/>
-       <button type="submit" className="border-solid border-2 bg-gray-300">Enviar</button>
+       <input type="text" className="border border-current " onChange={(e) => name(e.target.value)} required/>
+       <select className="border-solid  mx-3" onClick={(e) => {setEscolha(e.target.value)}}>
+       <option hidden>Escolha algum</option>
+       <option>Personagem</option>
+       <option>Planeta</option>
+       <option>naves estelares</option>
+       </select>
+       <button type="submit" className="border-solid bg-gray-300 hover:bg-slate-400">Enviar</button>
        </form>
+       </div>
     </>
   );
 }
