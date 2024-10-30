6cb72f902a0175810077ad5f08c444058c878972
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-29 21:02:09 -0300

    Atualizado para aparecer de acordo com a escolha

--- src/Componentes/Content/Content.jsx
+++ src/Componentes/Content/Content.jsx
@@ -1,25 +1,90 @@
-function Content({ corpo, removeLista }) {
+function Content({ corpo, removeLista, escolha }) {
   return (
     <>
-      {corpo.map((Personagens) => {
-        return (
-          <div
-            key={Personagens.created}
-            className="my-4 border-solid border-2 p-6 mx-auto w-96 grid gap-2 text-left shadow-lg black"
-          >
-            <div className="flex justify-end">
-              <button className="border-solid border-2 bg-gray-300 px-3 py-2 text-lg rounded-2xl" onClick={(e) => removeLista(Personagens.created)}>X</button>
+      {corpo.map((sobre) => {
+        if (sobre.edited == "Personagem") {
+          return (
+            <div
+              key={sobre.created}
+              className="my-4 border-solid border-2 p-6 mx-auto w-96 grid gap-2 text-left shadow-lg black"
+            >
+              <div className="flex justify-end">
+                <button
+                  className="border-solid border-2 bg-gray-300 px-3 py-2 text-lg rounded-2xl"
+                  onClick={(e) => removeLista(sobre.created)}
+                >
+                  X
+                </button>
+              </div>
+              <p className="font-bold">Name: {sobre.name}</p>
+              <p>Birth Year: {sobre.birth_year}</p>
+              <p>Skin Color: {sobre.skin_color}</p>
+              <p>Eye Color: {sobre.eye_color}</p>
+              <p>Gender: {sobre.gender}</p>
+              <p>Hair Color: {sobre.hair_color}</p>
+              <p>Height: {sobre.height}</p>
+              <p>Mass: {sobre.mass}</p>
             </div>
-            <p className="font-bold">Name: {Personagens.name}</p>
-            <p>Birth Year: {Personagens.birth_year}</p>
-            <p>Skin Color: {Personagens.skin_color}</p>
-            <p>Eye Color: {Personagens.eye_color}</p>
-            <p>Gender: {Personagens.gender}</p>
-            <p>Hair Color: {Personagens.hair_color}</p>
-            <p>Height: {Personagens.height}</p>
-            <p>Mass: {Personagens.mass}</p>
-          </div>
-        );
+          );
+        }
+        if (sobre.edited == "Planeta") {
+          return (
+            <div
+              key={sobre.created}
+              className="my-4 border-solid border-2 p-6 mx-auto w-96 grid gap-2 text-left shadow-lg black"
+            >
+              <div className="flex justify-end">
+                <button
+                  className="border-solid border-2 bg-gray-300 px-3 py-2 text-lg rounded-2xl"
+                  onClick={(e) => removeLista(sobre.created)}
+                >
+                  X
+                </button>
+              </div>
+              <p className="font-bold">Name: {sobre.name}</p>
+              <p>Climate: {sobre.climate}</p>
+              <p>Diameter: {sobre.diameter}</p>
+              <p>Gravity: {sobre.gravity}</p>
+              <p>Orbital Period: {sobre.orbital_period}</p>
+              <p>Population: {sobre.population}</p>
+              <p>Rotation Period: {sobre.rotation_period}</p>
+              <p>Surface Water: {sobre.surface_water}</p>
+              <p>terrain: {sobre.terrain}</p>
+            </div>
+          );
+        }
+        if (sobre.edited == "naves estelares") {
+          return (
+            <div
+              key={sobre.created}
+              className="my-4 border-solid border-2 p-6 mx-auto w-96 grid gap-2 text-left shadow-lg black"
+            >
+              <div className="flex justify-end">
+                <button
+                  className="border-solid border-2 bg-gray-300 px-3 py-2 text-lg rounded-2xl"
+                  onClick={(e) => removeLista(sobre.created)}
+                >
+                  X
+                </button>
+              </div>
+              <p className="font-bold">Name: {sobre.name}</p>
+              <p>MGLT: {sobre.MGLT}</p>
+              <p>Cargo Capacity: {sobre.cargo_capacity}</p>
+              <p>Consumables: {sobre.consumables}</p>
+              <p>Cost in Credits: {sobre.cost_in_credits}</p>
+              <p>Crew: {sobre.crew}</p>
+              <p>Hyperdrive Rating: {sobre.hyperdrive_rating}</p>
+              <p>Length: {sobre.length}</p>
+              <p>Manufacturer: {sobre.manufacturer}</p>
+              <p>Max Atmosphering Speed: {sobre.max_atmosphering_speed}</p>
+              <p>Model: {sobre.model}</p>
+              <p>Passengers: {sobre.passengers}</p>
+              <p>Starship Class: {sobre.starship_class}</p>
+
+
+            </div>
+          );
+        }
       })}
     </>
   );
