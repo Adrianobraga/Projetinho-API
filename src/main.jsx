0e50b30fc002cc1d161a11dba4be225412a0637d
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-30 16:49:00 -0300

    alertfy instalado

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './Componentes/App/App'
 import "./index.css"
+import 'alertifyjs/build/css/alertify.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
