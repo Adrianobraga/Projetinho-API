724883eb10fb88a44531a61d1ef222c2ef74c3b9
Adriano Braga <adrianobragamathias007@gmail.com>
2024-10-30 16:48:22 -0300

    testes para modificar o nome

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,25 +1,49 @@
-function Header({ name,pessoa,adicionarElemento,setEscolha }) {
+import alertify from "alertifyjs";
+function Header({ name, pessoa, adicionarElemento, setEscolha,setFixo,nome}) {
   const handleSubmit = (e) => {
-    adicionarElemento(pessoa)
-    e.preventDefault();
-    e.target.reset();
+    if (pessoa.name) {
+      adicionarElemento(pessoa);
+      e.preventDefault();
+      e.target.reset();
+      setFixo(nome);
+    } else {
+      e.preventDefault();
+      e.target.reset();
+      alertify.error("404 Error");
+      setFixo(nome);
+    }
   };
 
   return (
-    
     <>
-    <div className="flex justify-center bg-slate-200 h-40 py-16">
-      <form onSubmit={handleSubmit}>
-       <input type="text" className="border border-current " onChange={(e) => name(e.target.value)} required/>
-       <select className="border-solid  mx-3" onClick={(e) => {setEscolha(e.target.value)}}>
-       <option hidden>Escolha algum</option>
-       <option>Personagem</option>
-       <option>Planeta</option>
-       <option>naves estelares</option>
-       </select>
-       <button type="submit" className="border-solid bg-gray-300 hover:bg-slate-400">Enviar</button>
-       </form>
-       </div>
+      <div className="flex justify-center bg-slate-200 h-40 py-16">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="border border-current "
+            onKeyUp={(e) => name(e.target.value)}
+            required
+          />
+          <select
+            className="border-solid  mx-3"
+            onClick={(e) => setEscolha(e.target.value)}
+            required
+          >
+            <option hidden value="">
+              Escolha algum
+            </option>
+            <option value="Personagem">Personagem</option>
+            <option value="Planeta">Planeta</option>
+            <option value="naves estelares">naves estelares</option>
+          </select>
+          <button
+            type="submit"
+            className="border-solid bg-gray-300 hover:bg-slate-400"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
     </>
   );
 }
